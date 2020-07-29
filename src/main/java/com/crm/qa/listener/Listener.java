f39013f77f02fab30a25c0f9b07961935938e41a
Bamba DIA <bdia.sne@gmail.com>
2020-07-29 20:37:27 +0200

    Refactor TestNG parameters

--- src/main/java/com/crm/qa/listener/Listener.java
+++ src/main/java/com/crm/qa/listener/Listener.java
@@ -1,14 +1,11 @@
 package com.crm.qa.listener;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -16,15 +13,17 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.crm.qa.base.Base;
+import com.crm.qa.reports.ExtentReport;
 import com.crm.qa.utils.TestUtils;
 
-import com.crm.qa.reports.ExtentReport;
+
+
 
 public class Listener implements ITestListener {
 
+	Map<String, String> testNGparams;
 	TestUtils utils = new TestUtils();
 
 	public void onTestFailure(ITestResult result) {
@@ -40,9 +39,7 @@ public class Listener implements ITestListener {
 		// Here is the file format : screenshots/<browsermName>/<datetime /<testClass>
 		// /methodName.png
 
-		// Retrieve browser name from testng parameters
-		Map<String, String> testNGparams = new HashMap<String, String>();
-		testNGparams = result.getTestContext().getCurrentXmlTest().getAllParameters();
+		
 
 		Base base = new Base();
 
@@ -63,41 +60,34 @@ public class Listener implements ITestListener {
 			e.printStackTrace();
 		}
 
-		byte[] encoded = null;
-		try {
-			encoded = Base64.encodeBase64(FileUtils.readFileToByteArray(srcFile));
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			ExtentReport.getTest().fail("Test failed",
-					MediaEntityBuilder.createScreenCaptureFromPath(imagePath).build());
-
-			ExtentReport.getTest().fail("Test fail", MediaEntityBuilder
-					.createScreenCaptureFromBase64String(new String(encoded, StandardCharsets.UTF_8)).build());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 	}
-
+	
 	public void onTestStart(ITestResult result) {
+		// Retrieve browser name from testng parameters
+				testNGparams = new HashMap<String, String>();
+				testNGparams = result.getTestContext().getCurrentXmlTest().getAllParameters();
+			// Get the test Author
+			String author = testNGparams.get("author");
+			String browser = testNGparams.get("browser");
 		Base base = new Base();
-		ExtentReport.startTest(result.getName(), result.getMethod().getDescription()).assignCategory("Chrome")
-				.assignAuthor("Bamba Dia");
+		ExtentReport.startTest(result.getName(), result.getMethod().getDescription())
+				.assignCategory(browser)
+				.assignAuthor(author);
 	}
-
+	
+	
 	public void onTestSuccess(ITestResult result) {
+
 
 		ExtentReport.getTest().log(Status.PASS, "Test passed");
 	}
 
+	
 	public void onTestSkipped(ITestResult result) {
 		ExtentReport.getTest().log(Status.SKIP, "Test skipped");
 	}
 
+	
 	public void onFinish(ITestContext context) {
 		ExtentReport.getReporter().flush();
 	}
